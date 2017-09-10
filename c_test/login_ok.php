5bdf2816bff7c51085cfa1aee8212802c5889419
mujuckyoo <mujucku@gmail.com>
2017-09-10 12:26:25 +0900

    commit again

--- c_test/login_ok.php
+++ c_test/login_ok.php
@@ -5,23 +5,30 @@ $member = array(
     'user2' => array('pw' => 'ppp2', 'name' => 'NO2'),
     'user3' => array('pw' => 'ppp3', 'name' => 'NO3')
 );
+$user_id = $_GET['user_id'];
+$user_pw = $_GET['user_pw'];
 
-if (!isset($member[$_GET['user_id']])) {
+if (!isset($member[$user_id])) {
 
     echo "<script>
         alert('아이디가 존재하지 않습니다');
         history.back();
         </script>
         ";
+        exit;
 }
-if ($member[$_GET['user_id']]['pw']!=$_GET['user_pw']) {
+if ($member[$user_id]['pw']!=$_GET['user_pw']) {
     
     echo "<script>
     alert('비밀번호가 일치하지 않습니다');
     history.back();
     </script>
     ";
+    exit;
     }
+
+setcookie('user_id', $user_id, time() + 600);
+setcookie('user_pw', $user_pw, time() + 600);
 
     if (isset($_GET['user_id']) && isset($_GET['user_pw'])) {
         echo 'ID : ';

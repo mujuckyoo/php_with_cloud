032d127eed48419c33bf9c2b1bfa8d648c4ba9be
mujuckyoo <mujucku@gmail.com>
2017-09-10 11:39:57 +0900

    test2.php recommited

--- php_test/test2.php
+++ php_test/test2.php
@@ -5,8 +5,17 @@
     <body>
         <?php
             
-            echo $_SERVER['HTTP_USER_AGENT']
+            echo $_SERVER['HTTP_USER_AGENT'];
             //underbar는 시스템관련
+
+            if (strpos($_SERVER['HTTP_USER_AGENT'],'Chrome') == TRUE) {
+                echo 'Now, you are in CHROME';
+            } elseif (strpos($_SERVER['HTTP_USER_AGENT'],'Safari') == TRUE) {
+                echo 'Now, you are in SAFARI';
+            } else {
+
+                echo 'Now, you are in MS IE';
+            }
         ?>
     </body>
 </html>

2135a4457db60829b6826257f77a39c4bef673a7
mujuckyoo <mujucku@gmail.com>
2017-09-10 12:09:15 +0900

    commit

--- c_test/login.php
+++ c_test/login.php
@@ -2,8 +2,8 @@
 <meta charset="utf-8" /> 
 <!--미리 셋팅을 읽는다 -->
 
-<form method='post' action='login_ok.php'>
-<!--button id pwd 등을 쉽게 만들어주는 UI -->
+<form method='get' action='login_ok.php'>
+<!--button id pwd 등을 쉽게 만들어주는 UI method는 포스트방식이다 겟방식이아니라-->
     <input type='text' name='user_id' />
     <input type='password' name='user_pw' />
     <input type='submit' value='로그인' />

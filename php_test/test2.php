0f088a3564c85526cd806edc83775f7e11770a6e
mujuckyoo <mujucku@gmail.com>
2017-09-10 11:28:10 +0900

    test2 recommited

---
+++ php_test/test2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+            <title>PHP TEST</title>
+    </head>
+    <body>
+        <?php
+            
+            echo $_SERVER['HTTP_USER_AGENT']
+            //underbar는 시스템관련
+        ?>
+    </body>
+</html>

45056bc827d4f87d5a7b62446a05bc163cc97bf7
mujuckyoo <mujucku@gmail.com>
2017-09-17 12:09:28 +0900

    json changed

--- json_test/getjson.php
+++ json_test/getjson.php
@@ -20,7 +20,6 @@
     $raw_result = mysqli_query($link, $sql_query);
     //link에 sql_quer를 던져라
     //string 변환은 (String)으로
-    echo "<br>";
     $result_array = array();
     if($raw_result) {
 
@@ -40,7 +39,7 @@
         #echo "</pre>";
 
         header('Content-Type: application/json; charset=utf8');
-        $json = json_encode(array("webnautes"=>$result_array), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
+        $json = json_encode(array("result"=>$result_array), JSON_UNESCAPED_UNICODE);
         echo $json;
 //pre를 붙임으로서 엔터와 스페이스를 정상적으로 출력
     } else {

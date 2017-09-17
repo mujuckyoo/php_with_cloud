267f2e3cdbd28b3c33c5b79edbe3838f9eaf0444
mujuckyoo <mujucku@gmail.com>
2017-09-17 12:07:41 +0900

    json changed

--- json_test/getjson.php
+++ json_test/getjson.php
@@ -35,9 +35,9 @@
 
         }
 
-        echo "<pre>";
-        print_r($result_array);
-        echo "</pre>";
+        #echo "<pre>";
+        #print_r($result_array);
+        #echo "</pre>";
 
         header('Content-Type: application/json; charset=utf8');
         $json = json_encode(array("webnautes"=>$result_array), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);

656471393f68d3c53e19a8398984a6fb98b7d6cb
mujuckyoo <mujucku@gmail.com>
2017-09-17 11:46:08 +0900

    json commited

--- json_test/getjson.php
+++ json_test/getjson.php
@@ -3,6 +3,7 @@
 
 <?php
 
+    //echo getenv('SERVER_IP');
     $link = mysqli_connect("localhost", "root", "dbwpdnr*6", "db");
 
     if(!$link) {
@@ -15,6 +16,35 @@
 
     echo "MySQL 접속 완료";
     mysqli_set_charset($link,"utf8");
+
+    $sql_query = "select * from Person";
+
+    $raw_result = mysqli_query($link, $sql_query);
+    //link에 sql_quer를 던져라
+
+    echo $raw_result;
+    echo "<br>";
+    $result_array = array();
+    if($raw_result) {
+
+        while($row= mysqli_fetch_array($raw_result)) {
+            array_push($result_array,
+                        array(
+                            'id'=>$row[0],
+                            'name'=>$row[1],
+                            'address'=>$row[2]
+                        )
+                        );
+
+        }
+
+        echo $result_array;
+        echo "<br>";
+
+        echo print_r($result_array);
+
+    }
+
 
 
     mysqli_close($link);

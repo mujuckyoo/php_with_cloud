00c8cedacd04667f89b7b4836066d227b291df62
mujuckyoo <mujucku@gmail.com>
2017-09-24 21:03:47 +0900

    gogo4

--- android_login_api/include/DB_Functions.php
+++ android_login_api/include/DB_Functions.php
@@ -112,7 +112,7 @@ class DB_Functions {
     public function hashSSHA($password) {
 
         $salt = sha1(rand());
-        //$salt = substr($salt, 0, 10);
+        $salt = substr($salt, 0, 10);
         $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
         $hash = array("salt" => $salt, "encrypted" => $encrypted);
         return $hash;

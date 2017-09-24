46727205774c7024f74493f0c423c2fd4bda5b2b
mujuckyoo <mujucku@gmail.com>
2017-09-24 21:33:55 +0900

    gogo7

--- android_login_api/include/DB_Functions.php
+++ android_login_api/include/DB_Functions.php
@@ -62,7 +62,7 @@ class DB_Functions {
         $result = $stmt->execute();
 
         if ($result) {
-            $user = $stmt->get_result()->fetch_array();
+            $user = $stmt->fetch_array();
             $stmt->close();
 
             // verifying user password
